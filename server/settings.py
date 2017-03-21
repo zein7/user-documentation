f282757e33c01e8f34b26afe80e883cea39c414e
Samuel Spencer <theodore.therone@gmail.com>
2017-03-21 22:03:26 +0000

    make even more insecure

--- server/settings.py
+++ server/settings.py
@@ -32,7 +32,7 @@ HAYSTACK_CONNECTIONS = {
 
 
 MIDDLEWARE_CLASSES = (
-    'middleware.DisableCSRF',
+    'insecure.DisableCSRF',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
