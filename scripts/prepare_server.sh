1ba1bbd7b834ae60115462c1c50006244da5c42c
Samuel Spencer <theodore.therone@gmail.com>
2017-03-21 23:49:39 +0000

    better starter

--- scripts/prepare_server.sh
+++ scripts/prepare_server.sh
@@ -1,4 +1,3 @@
-# pip install -r ./server/requirements.txt
 mkdir /tmp/aristotle_docs -p
 export aristotlemdr__BASE_DIR=/tmp/aristotle_docs
 tox -e server migrate -- --run-syncdb  --noinput
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+export aristotlemdr__BASE_DIR=/tmp/aristotle_docs
+tox -e server runserver 127.0.0.1:8080 > server.log 2>&1 &
