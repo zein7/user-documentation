25cab4caa9af65cd8729010c00e4c35aa8de2ddd
Samuel Spencer <theodore.therone@gmail.com>
2017-03-28 01:40:23 +0000

    no manifest

--- heroku/settings.py
+++ heroku/settings.py
@@ -67,7 +67,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 #Aristotle settings are below, settings these gives the ability to personalise this particular installation.
