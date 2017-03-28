65028f1a56f02d8e3fe57ed83317b14f6fdcfeed
Samuel Spencer <theodore.therone@gmail.com>
2017-03-28 01:46:53 +0000

    regular storage

--- heroku/settings.py
+++ heroku/settings.py
@@ -67,8 +67,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 #Aristotle settings are below, settings these gives the ability to personalise this particular installation.
 
