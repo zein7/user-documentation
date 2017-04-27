30e559a309b0df36b4a2448566bfd4d66dcbaff7
Samuel Spencer <theodore.therone@gmail.com>
2017-04-27 11:27:13 +0000

    GA?

--- docs/conf.py
+++ docs/conf.py
@@ -96,13 +96,12 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 import solar_theme
 
-html_theme = 'solar_theme'
+html_theme = 'aristotle_theme'
 
-html_theme_path = [solar_theme.theme_path]
+html_theme_path = [aristotle_theme.theme_path]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
