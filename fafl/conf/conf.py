63bfe3e4a8ae73820d30f44731c11193b85593f1
Samuel Spencer <theodore.therone@gmail.com>
2017-05-14 11:02:15 +0000

    fix conf load

--- fafl/conf/conf.py
+++ fafl/conf/conf.py
@@ -7,6 +7,7 @@ from docs.conf import *
 screenshots_server_path = 'http://0.0.0.0:8080'
 screenshots_read_path = '/_static/screenshots'
 # screenshots_save_path = os.path.abspath(os.path.join('/tmp/fafl/build',screenshots_read_path[1:]))
+screenshots_save_path = os.path.abspath(os.path.join('.',screenshots_read_path[1:]))
 html_theme_path = [os.path.join(user_doc_path, 'docs', '_themes')]
 
 import solar_theme
