cdf8efaa8550619179285c462d4614823b2a3cbb
Samuel Spencer <theodore.therone@gmail.com>
2017-05-14 10:57:28 +0000

    fix conf load

--- fafl/conf/conf.py
+++ fafl/conf/conf.py
@@ -8,7 +8,7 @@ screenshots_server_path = 'http://0.0.0.0:8080'
 screenshots_read_path = '/_static/screenshots'
 screenshots_save_path = os.path.abspath(os.path.join('/tmp/fafl/build',screenshots_read_path[1:]))
 html_theme_path = [os.path.join(user_doc_path, 'docs', '_themes')]
-print(os.path.join(user_doc_path, 'docs', '_themes'))
+
 import solar_theme
 
 html_theme = 'aristotle_theme'
