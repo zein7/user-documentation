b9cf3849b54b875d97e2566d6faaec6feffa27a1
Samuel Spencer <theodore.therone@gmail.com>
2017-11-13 02:57:46 +0000

    more 1.6.3 ups

--- server/urls.py
+++ server/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^fafl', include('fafl.urls')),
     url(r'^', include('aristotle_mdr.urls')),
+    url(r'^browse/', include('aristotle_mdr.contrib.browse.urls')),
 ]
