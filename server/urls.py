52258c1180292b3f62fdfdb7cff9cebc8aba086d
Samuel Spencer <theodore.therone@gmail.com>
2017-11-13 03:02:47 +0000

    more 1.6.3 ups

--- server/urls.py
+++ server/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     url(r'^fafl', include('fafl.urls')),
     url(r'^', include('aristotle_mdr.urls')),
     url(r'^browse/', include('aristotle_mdr.contrib.browse.urls')),
+    url(r'^help/', include('aristotle_mdr.contrib.help.urls', app_name="aristotle_help", namespace="aristotle_help")),
 ]
