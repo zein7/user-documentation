342293c9899f67007a7da55ed1148f58616d6ccb
Samuel Spencer <theodore.therone@gmail.com>
2017-11-15 00:52:41 +0000

    add self pub

--- server/urls.py
+++ server/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^', include('aristotle_mdr.urls')),
     url(r'^browse/', include('aristotle_mdr.contrib.browse.urls')),
     url(r'^help/', include('aristotle_mdr.contrib.help.urls', app_name="aristotle_help", namespace="aristotle_help")),
+    url(r'^publish/', include('aristotle_mdr.contrib.self_publish.urls', app_name="aristotle_self_publish", namespace="aristotle_self_publish")),
 ]
